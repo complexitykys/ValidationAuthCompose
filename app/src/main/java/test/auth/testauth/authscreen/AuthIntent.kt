bed4cb7dffdb570bc15add67c0297e9e456fb31c
Matthew <conly9818@gmail.com>
2024-05-31 18:38:41 +0300

    Replaced data class with value class for AuthIntent implementations.

--- app/src/main/java/test/auth/testauth/authscreen/AuthIntent.kt
+++ app/src/main/java/test/auth/testauth/authscreen/AuthIntent.kt
@@ -2,11 +2,17 @@ package test.auth.testauth.authscreen
 
 sealed interface AuthIntent : Intent
 
-data class ChangeFullName(val newFullName: String) : AuthIntent
-data class ChangeUsername(val newUsername: String) : AuthIntent
-data class ChangeDateOfBirth(val newDateOfBirth: String) : AuthIntent
-data class ChangePassword(val newPassword: String) : AuthIntent
-data class ChangeConfirmPassword(val newPassword: String) : AuthIntent
-data class ChangeButtonState(val inEnabled: Boolean) : AuthIntent
-data object TogglePasswordVisibility: AuthIntent
-data object ToggleConfirmPasswordVisibility: AuthIntent
+@JvmInline
+value class ChangeFullName(val newFullName: String) : AuthIntent
+@JvmInline
+value class ChangeUsername(val newUsername: String) : AuthIntent
+@JvmInline
+value class ChangeDateOfBirth(val newDateOfBirth: String) : AuthIntent
+@JvmInline
+value class ChangePassword(val newPassword: String) : AuthIntent
+@JvmInline
+value class ChangeConfirmPassword(val newPassword: String) : AuthIntent
+@JvmInline
+value class ChangeButtonState(val inEnabled: Boolean) : AuthIntent
+data object TogglePasswordVisibility : AuthIntent
+data object ToggleConfirmPasswordVisibility : AuthIntent
